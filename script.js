e77bc30f240e402a7826843417bed81527e11685
IshtiaQ_ <115026795+IshtiaqIslamHimel@users.noreply.github.com>
2024-08-27 19:06:39 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
+  
+  var copy = document.querySelector(".logos-slide").cloneNode(true);
+  document.querySelector(".logos").appendChild(copy);
